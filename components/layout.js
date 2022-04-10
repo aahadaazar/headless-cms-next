578690b4d3cfa785941283e25dee497700955caa
Aahad <aahad@sudofy.com>
2022-04-10 18:41:41 +0500

    added head element

--- components/layout.js
+++ components/layout.js
@@ -1,25 +1,32 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function Layout({ children }) {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <header className='bg-sky-900 py-4'>
-        <div className=' text-blue-50 container mx-auto flex justify-center'>
-          <Link href='/'>
-            <a>👾 Home</a>
-          </Link>
-          <span className='mx-auto'>A NextJS Blog</span>{' '}
-          <Link href='/contentful/blogs'>
-            <a>Contentful Blogs 🤖</a>
-          </Link>
-        </div>
-      </header>
-      <main className='bg-slate-300 py-6 px-5 flex-1'>{children}</main>
-      <footer className='bg-sky-900 py-4'>
-        <div className='container mx-auto flex justify-center text-blue-50'>
-          &copy; Aahad Aazar
-        </div>
-      </footer>
-    </div>
+    <>
+      <Head>
+        <title>NextJS Blog</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className='flex flex-col min-h-screen'>
+        <header className='bg-sky-900 py-4'>
+          <div className=' text-blue-50 container mx-auto flex justify-center'>
+            <Link href='/'>
+              <a>👾 Home</a>
+            </Link>
+            <span className='mx-auto'>A NextJS Blog</span>{' '}
+            <Link href='/contentful/blogs'>
+              <a>Contentful Blogs 🤖</a>
+            </Link>
+          </div>
+        </header>
+        <main className='bg-slate-300 py-6 px-5 flex-1'>{children}</main>
+        <footer className='bg-sky-900 py-4'>
+          <div className='container mx-auto flex justify-center text-blue-50'>
+            &copy; Aahad Aazar
+          </div>
+        </footer>
+      </div>
+    </>
   );
 }
